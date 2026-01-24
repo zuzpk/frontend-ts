2bf5a670c5181bead3cbae1239633e5466fe7f6a
Kamran Wajdani <hello@zuz.com.pk>
2026-01-24 20:31:14 +0500

    Update: NextJs ^16.1.4 & @zuzjs/ui ^0.10.4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,5 +11,6 @@ export default function Home() {
   return <Box as={`w:100vw flex aic jcc h:90vh cols gap:15`}>
       <Text as={`text-clip bgi:gradient-to-right-$primary-$secondary s:36 b:900`}>@zuzjs/ui</Text>
       <Text as={`s:18`}>Get started by editing <code>src/app/page.tsx</code>.</Text>  
+      
     </Box>
 }
